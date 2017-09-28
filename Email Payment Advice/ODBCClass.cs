02755d764b41759cff5e83ea0eaaf32241a999d5
Ron Mittelman <RMittelman@gmail.com>
2017-09-27 20:03:48 -0700

    Progress commit.  Added BCC, updating student.

---
+++ Email Payment Advice/ODBCClass.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Odbc;
+
+namespace Email_Payment_Advice
+{
+    class ODBCClass : IDisposable
+    {
+        OdbcConnection oConnection;
+        OdbcCommand oCommand;
+
+        public ODBCClass(string DataSourceName)
+        {
+            //Instantiate the connection
+            oConnection = new OdbcConnection("Dsn=" + DataSourceName);
+            try
+            {
+                //Open the connection
+                oConnection.Open();
+                Console.WriteLine("The connection is established with the database");
+
+            }
+            catch (OdbcException caught)
+            {
+                Console.WriteLine(caught.Message);
+                Console.Read();
+            }
+        }
+
+        /// <summary>
+        /// Execute SQL query
+        /// </summary>
+        /// <param name="Query">The SQL command to execute</param>
+        /// <returns></returns>
+        public OdbcCommand GetCommand(string Query)
+        {
+            oCommand = new OdbcCommand();
+            oCommand.Connection = oConnection;
+            oCommand.CommandText = Query;
+            return oCommand;
+        }
+
+        /// <summary>
+        /// Call stored procedure
+        /// </summary>
+        /// <param name="storedProcName">Procedure name including (?,?,...) for parameters.</param>
+        /// <param name="parms">Array of KeyValuePairs, each containing param name and param value.</param>
+        /// <returns></returns>
+        public OdbcCommand GetCommand(string storedProcName, KeyValuePair<string, string>[] parms)
+        {
+            oCommand = new OdbcCommand("{call " + storedProcName + "}", oConnection);
+            oCommand.CommandType = CommandType.StoredProcedure;
+            foreach (var kvp in parms)
+            {
+                oCommand.Parameters.AddWithValue(kvp.Key, kvp.Value);
+            }
+            return oCommand;
+        }
+
+
+
+        public void Dispose()
+        {
+            oConnection.Close();
+        }
+    }
+}
